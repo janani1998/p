2dc5f57ae4b62865d20aaa27f9a593ee3016353e
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-22 10:11:26 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,12 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int n=5;
+	  int a[]={-1,-2,-3,-4,-5};
+	  for(int i=0;i<n;i++)
+	  {
+	  	System.out.println(a[i]+" "+i);
+	  }
+	}
+}
