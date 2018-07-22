0b548e79d694513bfda0e5b0a4acd70a0ad91624
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-22 11:40:14 +0530

    Create subtwotime.java

---
+++ subtwotime.java
@@ -0,0 +1,13 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int h1=3;
+		int h2=4;
+		int min1=42;
+		int min2=80;
+		int a=Math.abs(h1-h2);
+		int b=Math.abs(min1-min2);
+		System.out.print(a+" "+b);
+	}
+}
