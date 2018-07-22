d36e73aacc18e6d4fd579023d8a7a216cc2612e8
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-22 12:20:39 +0530

    Create numeric.java

---
+++ numeric.java
@@ -0,0 +1,11 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s="v234sh&**";
+		if(Character.isDigit(s.charAt(0)))
+			System.out.print("yes");
+		else
+			System.out.print("No");
+	}
+}
